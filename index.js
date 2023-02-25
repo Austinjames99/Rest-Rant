53251b4859ca3d3c016899f3ec844fe2131fb31f
Austinjames99 <james.aj.austin@gmail.com>
2023-02-24 22:22:30 -0500

    adding places view with mock data

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page Not Found</h1>')
+    res.render('error')
 })
 
 app.listen(process.env.PORT)
